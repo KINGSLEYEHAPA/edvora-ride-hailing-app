3e837796450d52c25e5376675acb47294fffb931
KINGSLEYEHAPA <kessity09@gmail.com>
2022-02-20 14:46:16 +0100

    done with Filter static container

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -3,30 +3,38 @@ import { VscTriangleDown, VscTriangleUp } from "react-icons/vsc";
 
 const Filter = () => {
   return (
-    <div className="absolute lg:w-48 lg:h-44 lg:flex lg:flex-col lg:top-6 lg:right-2  bg-neutral-900 lg:justify-between lg:space-y-5 lg:p-6 lg:shadow-2xl">
-      <h2 className="text-trueGray-400  text-md">Filters</h2>
-      <hr className="text-trueGray-400"></hr>
+    <div className="absolute lg:w-48 lg:h-44 lg:flex lg:flex-col lg:top-7 lg:right-2 shadow  bg-neutral-900 lg:justify-between lg:space-y-5 lg:py-4 px-5  rounded-xl">
+      <h2 className="text-neutral-700  text-md">Filters</h2>
+      <hr className="text-neutral-700 border-t-3 border-neutral-700 relative bottom-3"></hr>
       <div className="flex relative">
-        <select name="state" id="state" className=" w-full">
+        <select
+          name="state"
+          id="state"
+          className=" w-full bg-neutral-700 p-1 text-sm outline-none rounded"
+        >
           <option value="volvo">Edo</option>
           <option value="saab">Lagos</option>
         </select>
-        <span className="absolute text-trueGray-500 top-0.5 right-1">
+        <span className="absolute text-neutral-700 top-0.5 right-1">
           <VscTriangleUp />
         </span>
-        <span className="absolute text-trueGray-500 top-0.5 right-1">
+        <span className="absolute text-neutral-700 top-0.5 right-1">
           <VscTriangleDown />
         </span>
       </div>
       <div className="flex relative">
-        <select name="city" id="city" className=" w-full">
+        <select
+          name="city"
+          id="city"
+          className=" w-full bg-neutral-700 p-1 text-sm outline-none rounded"
+        >
           <option value="volvo">Warri</option>
           <option value="saab">Lekki</option>
         </select>
-        <span className="absolute text-trueGray-500 top-0.5 right-1">
+        <span className="absolute text-neutral-700 top-0.5 right-1">
           <VscTriangleUp />
         </span>
-        <span className="absolute text-trueGray-500 top-0.5 right-1">
+        <span className="absolute text-neutral-700 top-0.5 right-1">
           <VscTriangleDown />
         </span>
       </div>
